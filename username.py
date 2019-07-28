ea1b4e822467f9aeb2afce5554c3cacd33cb1648
Jaguar <48777426+Jaguar-515@users.noreply.github.com>
2019-07-28 10:47:01 -0700

    Delete username.py

--- username.py
+++
@@ -1,9 +0,0 @@
-import robloxapi
-rbx = robloxapi.client() 
-
-username = input("Enter in a username: ") # Enter in a username 
-rbx.Auth.IsUsernameTaken(username) # Checks if username is taken or not
-if (rbx.Auth.IsUsernameTaken(username)): # If username is taken, do this
-    print("Username is taken")
-else: # If username is not taken, do this
-    print("Username is not taken")
