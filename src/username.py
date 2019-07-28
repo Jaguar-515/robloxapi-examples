be950c2bf35b9a4049b9b5d31bc8821039fe7ccc
ira <38579267+iranathan@users.noreply.github.com>
2019-07-28 19:50:11 +0200

    Update username.py

--- src/username.py
+++ src/username.py
@@ -2,7 +2,7 @@ import robloxapi
 rbx = robloxapi.client() 
 
 username = input("Enter in a username: ") # Enter in a username 
-rbx.Auth.IsUsernameTaken(username) # Checks if username is taken or not
+
 if (rbx.Auth.IsUsernameTaken(username)): # If username is taken, do this
     print("Username is taken")
 else: # If username is not taken, do this
