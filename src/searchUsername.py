a4cbc74f2c7c2d7d9a07d1431665e77a4948636e
Jaguar <48777426+Jaguar-515@users.noreply.github.com>
2019-08-14 11:50:20 -0700

    Update searchUsername.py
    You can now view the results in a JSON file

--- src/searchUsername.py
+++ src/searchUsername.py
@@ -4,4 +4,5 @@ rbx = robloxapi.client()
 
 keyword = input("Please enter in a keyword to search users: ") # Enter in a keyword
 keywordJSON = json.dumps(rbx.User.searchUsers(keyword)) # Convert this into JSON
-print(keywordJSON)
+jsonFile = open("results.json", "w")
+jsonFile.write(keywordJSON)
