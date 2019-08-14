132b9078784efaa282a15039ede8f825db24763e
Jaguar <48777426+Jaguar-515@users.noreply.github.com>
2019-08-14 11:36:36 -0700

    Made result convert into JSON

--- src/getProfile.py
+++ src/getProfile.py
@@ -1,5 +1,6 @@
 import robloxapi
+import json
 rbx = robloxapi.client()
 
 userId = input("Enter in a userId: ") # Enter in a userId
-print(rbx.User.getProfile(userId)) # Returns a dictionary
+userIdJSON = json.dumps(rbx.User.getProfile(userId)) # Convert this into JSON
